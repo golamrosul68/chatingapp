e2d756c7880c25274264a96c633b4f7fee1a9786
golamrosul68 <golamrosul68990@gmail.com>
2025-05-15 00:48:25 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import NotFound from "./pages/notFound";
 import Home from "./pages/Home";
