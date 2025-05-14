4dd3d56152afe7d4b31f0ef63df0e7863a8d8459
golamrosul68 <golamrosul68990@gmail.com>
2025-05-14 19:34:11 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
 
 const App = () => {
   return (
-    <div></div>
+      <RouterProvider router={router} />
   )
 }
 
