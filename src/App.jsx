d0e33433de4f56a84c74dc761a68011d8aba6f51
golamrosul68 <golamrosul68990@gmail.com>
2025-05-14 22:12:44 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import NotFound from "./pages/notFound";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home/>,
+  },
+  {
+    path: "*",
+    element: <NotFound/>
   },
 ]);
 
 const App = () => {
-  return (
-      <RouterProvider router={router} />
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
